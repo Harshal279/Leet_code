dfcc5bad7e04fd28a902ffdc9b1c39f8cc51f12b
Harshal279 <tanmayghoradkar1997@gmail.com>
2024-09-30 23:44:48 +0530

    231.cpp

---
+++ 231.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <cmath>
+using namespace std;
+int main () {
+    int inp;
+    cin >> inp;
+    for (int i = 0;i<= 30;i++){
+            int ans=pow(2,i);
+            if (ans==inp){
+                cout << "true" ;}}
+        cout << "false";}
