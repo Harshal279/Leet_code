04f79a5904353c1d22ae2949cc59ac84bc0f3be7
Harshal279 <tanmayghoradkar1997@gmail.com>
2024-12-09 18:43:44 +0530

    7

--- 7.cpp
+++ 7.cpp
@@ -1,18 +1,20 @@
 # include <iostream>
-using namespace std ;
-int main ()
-{
-    int inp , num = 0 , sum = 0 ;
-    cin >> inp ;
-    while (inp != 0)
-    {
-        num = inp % 10 ;
-        if ((sum > INT_MAX/10) || (sum < INT_MIN/10))
-        {
-            return 0;
-        }
-        sum = ( sum * 10 ) + num ;
-        inp = inp / 10 ;
+using namespace std ; 
+int main () {
+    int n = 123, ans =0 ;
+    while (n != 0){
+        int digit  = n%10 ;
+        if (ans > (INT_MAX/10) || ans < (INT_MIN/10)){
+            cout << 0;
+        };
+        ans = (ans *10) + digit ;
+        n =n /10 ;
     }
-    cout << sum << endl;
+    cout << ans ; 
+    
+    
+
+
+     
+
 }
